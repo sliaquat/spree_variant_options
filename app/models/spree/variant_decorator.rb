0a9f8c77cc1cb1fc72a975fab1d7a40f91b2e7e8
Sanad Liaquat <sanadhussain@gmail.com>
2015-05-11 11:38:21 +0500

    Changes for canvas

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -3,12 +3,15 @@ Spree::Variant.class_eval do
   include ActionView::Helpers::NumberHelper
 
   def to_hash
-    actual_price  = self.price
+    actual_price  = self.orig_price_in(Spree::Config[:currency]).money
+    sale_price = self.sale_price ? self.price_in(Spree::Config[:currency]).money : nil
+
     #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
     {
       :id    => self.id,
       :in_stock => self.in_stock?,
-      :price => number_to_currency(actual_price)
+      :price => actual_price,
+      :sale_price => sale_price
     }
   end
 
